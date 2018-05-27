7fb0a2242b781fb63c9a18b21d13c6830a802f84
Nutan <nutan.nivate@gmail.com>
2018-05-27 21:09:34 +0530

    Execute script for build and packaging

---
+++ travis/build-android.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -v
+
+set -e
+
+# Build Ionic App for Android
+cordova platform add android --nofetch
+
+if [[ "$TRAVIS_BRANCH" == "travis" ]]
+then
+    echo "building apk for dev"
+    ionic cordova build android
+else
+    echo "building apk from prod"
+    ionic cordova build android --prod --release
+fi
